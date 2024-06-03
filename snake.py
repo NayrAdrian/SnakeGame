e6b2685243946fd1c1359c5395f8c8dffcedb74d
Ryan Adrian <NayrAdrian24@gmail.com>
2024-06-03 14:32:25 +0800

    updated the Snake class

--- snake.py
+++ snake.py
@@ -1,13 +1,15 @@
 from turtle import Turtle
 
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
 
 class Snake:
     def __init__(self):
-        starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
         self.segments = []
+        self.create_snake()
 
-        for position in starting_positions:
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
