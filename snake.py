cb176d8fde5892cbe043c4cc1ab8bdd5fd76e151
Ryan Adrian <NayrAdrian24@gmail.com>
2024-06-03 14:27:49 +0800

    Created Class and moved snake functions

---
+++ snake.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+class Snake:
+    def __init__(self):
+        starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+        self.segments = []
+
+        for position in starting_positions:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
